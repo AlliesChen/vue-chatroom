a8e33fe3ba24c0255829dde52788376fefd242af
YPChen <cyphuaenng@pm.me>
2022-10-07 01:57:14 +0800

    add styles to chat room

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,9 +21,21 @@ function joinRoom() {
     <!-- Lobby -->
     <div v-if="showChat === false" class="_translate-y flex-col gap-3">
       <h3 class="my-4 text-2xl">Join A Chat</h3>
-      <input class="px-1 py-2 rounded border _border-green bg-green-pale border-green" type="text" placeholder="Display Name..." v-model="username" />
-      <input class="px-1 py-2 rounded border _border-green bg-green-pale border-green" type="text" placeholder="Room ID..." v-model="room" />
-      <button class="py-2 rounded bg-green text-white" @click="joinRoom">Join A Room</button>
+      <input
+        class="px-1 py-2 rounded border _border-green bg-green-pale border-green"
+        type="text"
+        placeholder="Display Name..."
+        v-model="username"
+      />
+      <input
+        class="px-1 py-2 rounded border _border-green bg-green-pale border-green"
+        type="text"
+        placeholder="Room ID..."
+        v-model="room"
+      />
+      <button class="py-2 rounded bg-green text-white" @click="joinRoom">
+        Join A Room
+      </button>
     </div>
 
     <ChatRoom v-else :socket="socket" :username="username" :room="room" />
@@ -35,14 +47,22 @@ function joinRoom() {
 @use "./styles/library";
 
 ._translate-y {
-  transform: translateY(-25%)
+  transform: translateY(-25%);
 }
 
 ._border-green:focus,
 ._border-green:focus-within,
 ._border-green:focus-visible {
-  
   border-color: map.get(library.$app-colors, "blue-neutral");
   background-color: map.get(library.$app-colors, "blue-pale");
+}
+
+@media (prefers-color-scheme: dark) {
+  ._border-green:hover,
+  ._border-green:focus,
+  ._border-green:focus-within,
+  ._border-green:focus-visible {
+    color: map.get(library.$app-colors, "black");
+  }
 }
 </style>
