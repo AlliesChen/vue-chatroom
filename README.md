3a91a1c5cdf3c815f280829c0954de743af7a624
YPChen <cyphuaenng@pm.me>
2022-10-07 02:06:40 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chat Room
+
+This app refers to [the tutorial](https://youtu.be/NU-HfZY3ATQ), while I pick Vue as the JS framework, not React. Also, I use Sass rather than CSS as the tutorial does.
+
+## Host Server
+
+`npm run start`
+
+## Run Client
+
+`npm run dev`
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import http   from "http";
+import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
 
 interface UserSentMessage {
-    room: string
-    author: string
-    message: string
-    time: string
+  room: string;
+  author: string;
+  message: string;
+  time: string;
 }
 
 const app = express();
@@ -15,27 +15,27 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://127.0.0.1:5173",
-        methods: ["GET", "POST"],
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    })
-    socket.on("send_message", (data: UserSentMessage) => {
-        socket.to(data.room).emit("receive_message", data);
-    })
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    })
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+  socket.on("send_message", (data: UserSentMessage) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(3001, () => {
-    console.log("SERVER RUNNING");
-})
+  console.log("SERVER RUNNING");
+});
